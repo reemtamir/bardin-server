1a1d66bd9f88aee3eab7a8d1aa6ed09c339f05e7
Reem Tamir <reem.tamir36@gmail.com>
2023-05-19 16:49:42 +0300

    change server port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 const { connect } = require('./schema');
 const http = require('http').Server(app);
 const cors = require('cors');
